2fba3e1213c09e82c71e645a62542a7b2e0a7839
Jongha Lim <keshasha@gmail.com>
2019-12-24 19:09:40 +0100

    Updates

---
+++ precache-manifest.7b0512ce01c26fed5f6d35d6ac4b044c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cab2ce35d8562c9cecfafaaef2047e4",
+    "url": "/movie_react/index.html"
+  },
+  {
+    "revision": "c4f95654e69529621d60",
+    "url": "/movie_react/static/css/main.1387257c.chunk.css"
+  },
+  {
+    "revision": "efbb4a9fe5ab1885af8a",
+    "url": "/movie_react/static/js/2.4d424e3d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_react/static/js/2.4d424e3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4f95654e69529621d60",
+    "url": "/movie_react/static/js/main.1cf100a7.chunk.js"
+  },
+  {
+    "revision": "7060d86d863ed2a21614",
+    "url": "/movie_react/static/js/runtime-main.2c7fc2e4.js"
+  }
+]);
